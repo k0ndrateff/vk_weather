2f50d01f571ea7069ee181a3a42e051d2e52546e
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2023-04-17 18:06:22 +0300

    Исправление критического бага из-за некорректной версии React.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,7 @@ import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
 // Init VK  Mini App
-bridge.send("VKWebAppInit").then(r => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    , document.getElementById("root"));
-});
+bridge.send("VKWebAppInit");
+ReactDOM.render(<App />, document.getElementById("root"));
+
 
